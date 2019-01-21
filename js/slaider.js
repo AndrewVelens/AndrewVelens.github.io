e079b45ab7578dcae65065eabdf3d0fde602b04a
Andrew Velens <dzholkha@gmail.com>
2019-01-21 21:32:17 +0300

    21.01

---
+++ js/slaider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.stock-slaider').slick({
+  mobileFirst:true,
+  prevArrow: '<button class= "arrow prev"></button>',
+  nextArrow: '<button class= "arrow next"></button>',
+  });
+});
+
+
+
+    
