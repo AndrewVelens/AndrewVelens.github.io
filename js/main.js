d923f54f863c249759d7d4b1ca32bd95c8e7bc09
Andrew Velens <dzholkha@gmail.com>
2019-02-02 10:50:34 +0300

    #8

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 
-var c = 'white';
-
+var PrioirityColor;
 
 function Selected(a) {
 
     var label = a.value;
 
     if (label==1) {
-        c = 'red';
+        PrioirityColor = 'red';
          
     } else if (label==2) {
-        c='yellow';
+        PrioirityColor='yellow';
 
     } else if (label==3) 
 
-        c='green';
+        PrioirityColor='green';
 }
 
 
@@ -27,6 +26,8 @@ function test3(){
     var TaskDescription = form.querySelector('.TaskDescription');
     var priority = form.querySelector('.priority');
     var fields = form.querySelectorAll('.field');
+    var dateInf = form.querySelector('.dateInf');
+    
     
     
     var placeholderTask2= document.createElement('div');
@@ -42,17 +43,24 @@ function test3(){
     
     var taskPlaceholderPriorityClr2 = document.createElement('div');
     taskPlaceholderPriorityClr2.className = " placeholder-priority-clr";
-    taskPlaceholderPriorityClr2.style.backgroundColor = c;
+    taskPlaceholderPriorityClr2.style.backgroundColor = PrioirityColor;
     
      
     var PlaceholderDescription2 = document.createElement('div');
     PlaceholderDescription2.className = 'placeholder-description';
     
+    var dateInf2 = document.createElement('div');
+    dateInf2.className = 'placeholder-data';
+    
+    var CheckBox = document.createElement('input');
+    CheckBox.type = ('checkbox'); 
+    CheckBox.className = 'placeholder-status';
     
     
 
     placeholderTaskText.innerHTML = Task.value;
-    PlaceholderDescription2.innerHTML = 'nonono';
+    PlaceholderDescription2.innerHTML = TaskDescription.value;
+    dateInf2.innerHTML = dateInf.value;
     
     task.appendChild(taskPlaceholder2);
     taskPlaceholderPriority2.appendChild(taskPlaceholderPriorityClr2);
@@ -60,12 +68,11 @@ function test3(){
     taskPlaceholder2.appendChild(placeholderTask2);
     placeholderTask2.appendChild(placeholderTaskText);
     taskPlaceholder2.appendChild(PlaceholderDescription2);
+    taskPlaceholder2.appendChild(dateInf2);
+    taskPlaceholder2.appendChild(CheckBox);
     
-    
-    
-    
-    
-   
+    Task.value = "";
+    TaskDescription.value = "";
     
     
 }
@@ -77,7 +84,8 @@ document.getElementById("btn").onclick = test3;
 
   function check() {
   var pole1 = document.querySelector('.Task');
-  document.getElementById('btn').disabled = pole1.value ? false : "disabled";}
+  
+  document.getElementById('btn').disabled = pole1.value  ? false : "disabled";}
 
 
 
