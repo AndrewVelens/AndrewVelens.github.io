c12456f76c40c659262ba82663cb1634cf60d6a3
AndrewVelens <44371434+AndrewVelens@users.noreply.github.com>
2019-02-07 21:01:28 +0300

    07-02#2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
+var placeholderMass = document.querySelector('.placeholderMass');
+var placehol = placeholderMass.querySelectorAll('.placeholder');
 
-var placehol = document.querySelectorAll('.placeholder');
-
- window.onload = function (){
+window.onload = function change(){
     for(var i=0; i<placehol.length; i++)placehol[i].setAttribute("onclick", "location.href='http://www.example.com';");
-    for(var i=0; i<placehol.length; i++)placehol[i].style.cursor = "pointer";
-   
+    for(var i=0; i<placehol.length; i++)placehol[i].style.cursor = 'pointer';
 }
 
