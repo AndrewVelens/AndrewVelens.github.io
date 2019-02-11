07287ee2aceb6494b0c18c0637b26ad0747b124a
AndrewVelens <44371434+AndrewVelens@users.noreply.github.com>
2019-02-11 12:54:40 +0300

    11-02#1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var placeholderMass = document.querySelector('.placeholderMass');
 var placehol = placeholderMass.querySelectorAll('.placeholder');
 
 window.onload = function change(){
-    for(var i=0; i<placehol.length; i++)placehol[i].setAttribute("onclick", "location.href='http://www.example.com';");
+    for(var i=0; i<placehol.length; i++)placehol[i].setAttribute("onclick", "location.href='productpagetemplate.html';");
     for(var i=0; i<placehol.length; i++)placehol[i].style.cursor = 'pointer';
 }
 
